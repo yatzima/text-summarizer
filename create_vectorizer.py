5d6da29cc55448a4f4d78c0a1ef91b35b0bc6d83
Justin Ma <justinma@yatzima.local>
2021-04-02 15:54:29 +0200

    add streamlit app, script for creating vectorizer and pickled vectorizer-file

---
+++ create_vectorizer.py
@@ -0,0 +1,50 @@
+import os
+import re
+import json
+import spacy
+import pickle
+import time
+import numpy as np
+
+from tqdm import tqdm
+from sklearn.feature_extraction.text import TfidfVectorizer
+
+
+def main():
+    t0 = time.time()
+    print('loading spacy model')
+    nlp = spacy.load("en_core_web_md")
+    corpora = []
+    data_path = 'data'
+
+    print('loading data')
+    N = len(os.listdir(data_path))
+    for filename in tqdm(os.listdir(data_path)):
+        data = json.load(open(os.path.join(data_path, filename)))
+        text = data['text']
+        text = re.sub('\n', '', text)
+        text_sentences = nlp(text)
+        sentences = [sentence.text for sentence in text_sentences.sents]
+        corpora.extend(sentences)
+
+    print('fitting vectorizer')
+    tfidf = TfidfVectorizer(sublinear_tf=True,
+                           strip_accents='unicode',
+                           min_df=5,
+                           stop_words='english',
+                           ngram_range=(1,1))
+    tfidf.fit(corpora)
+
+    print('saving vectorizer')
+    with open('vectorizer.pkl', 'wb') as f:
+        pickle.dump(tfidf, f)
+
+    print('done')
+    t1 = time.time()
+    total = t1-t0
+    print(f'running this script took {np.round(total, 2)}s')
+
+
+if __name__ == "__main__":
+    main()
+
---
+++ streamlit_app.py
@@ -0,0 +1,68 @@
+import re
+import spacy
+import pickle
+import time
+import numpy as np
+import pandas as pd
+import networkx as nx
+import streamlit as st
+import matplotlib.pyplot as plt
+
+from sklearn.metrics.pairwise import cosine_similarity
+
+
+#@st.cache
+def load_vectorizer(filename='vectorizer.pkl'):
+    with open(filename, 'rb') as f:
+        vectorizer = pickle.load(f)
+    return vectorizer
+
+
+st.title("Text Summarizer")
+st.header("Automatic summarization is the process of shortening a set of data \
+computationally, to create a subset (a summary) that represents the most \
+important or relevant information within the original content.")
+
+st.sidebar.title("Settings")
+
+language  = st.sidebar.selectbox(
+    "Choose language",
+    ("English", "Swedish")
+)
+
+model_choice = st.sidebar.radio("Choose model", 
+        ["LexRank (Extraction-based)",
+        "TextRank (Extraction-based)", 
+        "Deep Learning (Abstraction-based)"])
+
+factor = st.sidebar.slider('Factor to summarize text.', 0, 100, 75)
+
+text  = st.text_area("Input your text here:", height=400) 
+
+# Load stuff
+vectorizer = load_vectorizer()
+nlp = spacy.load("en_core_web_md")
+
+if text:
+    text_sentences = nlp(text)
+    text = re.sub('\n', '', text)
+    sentences = [sentence.text for sentence in text_sentences.sents]
+    N = int(np.round((factor/100)*len(sentences)))
+    mapping = {i:sentence for i, sentence in enumerate(sentences)}
+    M = vectorizer.transform(sentences)
+    S = cosine_similarity(M)
+    np.fill_diagonal(S, 0)
+    adjacency_df = pd.DataFrame(S)
+    G = nx.convert_matrix.from_pandas_adjacency(adjacency_df)
+    G = nx.relabel_nodes(G, mapping)
+    scores = nx.pagerank(G)
+    score_df = pd.DataFrame(scores.items(), columns=['Descriptions', 'Score'])
+    indices = score_df.sort_values(by='Score', ascending=False)[:N].index.sort_values()
+    pd.set_option("display.max_colwidth", None)
+    summarized = score_df.iloc[indices]['Descriptions'].to_string(header=False, index=False)
+    summarized = re.sub('\n', '', summarized)
+    st.text("Summary of your text:")
+    st.write(summarized.strip())
+    st.balloons()
+
+
